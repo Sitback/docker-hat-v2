edb43e44c554faf62032e267a0bd2f1440abc584
Chinthaka Godawita <chin@sitback.com.au>
2015-11-16 17:50:48 +1100

    Added kingpin and basic flags.

--- main.go
+++ main.go
@@ -4,8 +4,44 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
+)
+
+var (
+	Name    string
+	Version string
+	Build   string
 )
 
 func main() {
-	fmt.Println("Hello")
+	pwd, pwdErr := os.Getwd()
+	if pwdErr != nil {
+		fmt.Println("Error: could not determine your current directory:", pwdErr.Error())
+		os.Exit(1)
+	}
+
+	var (
+		verbose = kingpin.
+			Flag("verbose", "Verbose mode.").
+			Short('v').
+			Bool()
+	)
+
+	// Setup '-h' as an alias for the help flag.
+	kingpin.CommandLine.HelpFlag.Short('h')
+
+	// Setup version printing.
+	kingpin.Flag("version", "Show version.").PreAction(kingpin.Action(func(*kingpin.ParseContext) error {
+		fmt.Printf("%v version %v, build %v\n", Name, Version, Build)
+		os.Exit(0)
+		return nil
+	})).Bool()
+
+	kingpin.Parse()
+
+	if *verbose {
+		fmt.Println("Hello, this is verbose mode.")
+		fmt.Println("The current directory is:", pwd)
+	}
 }
